74b911b9ebd0c3510d8ffcbca35ec020db4232d2
Plutaniano <lucasrodriguessilva@hotmail.com>
2021-02-10 09:34:12 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,126 @@
+from flask import Flask, request, render_template, redirect, url_for, flash
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
+from flask_sqlalchemy import SQLAlchemy
+from forms import *
+from validate import *
+from datetime import datetime, date
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from models import *
+
+@login_manager.user_loader
+def load_user(id):
+    user = db.session.query(Assessor).filter_by(id=id).first()
+    return user
+
+# ------------------------
+#        user pages
+# ------------------------
+@app.route('/')
+def home():
+    return render_template('pages/home.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('pages/sobre.html')
+
+@app.route('/pagamento')
+def pagamento():
+    return render_template('pages/pagamento.html')
+
+@app.route('/forgot')
+def forgot():
+    form = ForgotForm(request.form)
+    return render_template('forms/forgot.html', form=form)
+
+@app.route('/pj1', methods=['GET', 'POST'])
+@login_required
+def pj1():
+    if current_user.is_admin:
+        assessores = list(db.session.query(Assessor))
+    else:
+        assessores = [current_user]
+
+    form = QueryForm(request.form)
+
+    anos_meses = list(db.session.query(Investimentos.ano_mes).distinct())
+    for i in anos_meses:
+        s = i.ano_mes.strftime('%Y/%m')
+        form.ano_mes.choices.append((s, s))
+    
+    for i in assessores:
+        t = (i.id, 'A' + str(i.id) + ' - ' + i.nome)
+        form.assessores.choices.append(t)
+
+    if request.method == 'GET':
+        return render_template('pages/pj1.html', assessores=assessores, anos_meses=anos_meses, form=form)
+
+    if request.method == 'POST':
+
+        assessor = load_user(request.form.get('assessores'))
+        ano = int(request.form.get('ano_mes').split('/')[0])
+        mes = int(request.form.get('ano_mes').split('/')[1])
+        ano_mes = date(ano, mes, 1)
+        tabela = request.form.get('tabela')
+
+        data = db.session.query(Investimentos).filter_by(assessor_direto_codigo=assessor.id, ano_mes=ano_mes)
+        return render_template('pages/pj1.html', assessores=assessores, anos_meses=anos_meses, data=data, form=form, tabela=tabela)
+
+
+
+# ------------------------
+#      admin pages
+# ------------------------
+@app.route('/admin')
+@login_required
+def admin():
+    return render_template('pages/admin.html', registerform=RegisterForm(request.form))
+
+@app.route('/upload', methods=['POST'])
+@login_required
+def upload():
+    return 'a'
+
+@app.route('/adicionarassessor', methods=['POST'])
+@login_required
+def adicionarassessor():
+    form = RegisterForm(request.form)
+    db.session.add(Assessor(id=form.id.data, nome=form.name.data, email=form.email.data))
+    db.session.commit()
+    flash('Assessor adicionado com sucesso.')
+    return redirect(url_for('admin'))
+
+@app.route('/comissoes')
+@login_required
+def comissoes():
+    assessores = db.session.query(Assessor)
+    return render_template('pages/comissoes.html', assessores=assessores)
+
+
+# ------------------------
+#           auth
+# ------------------------
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = LoginForm(request.form)
+    if form.validate_on_submit():
+        user = load_user(form.id.data)
+        if user is None or user.password != form.password.data:
+            flash('Credenciais inválidas')
+            return redirect(url_for('login'))
+        login_user(user)                                        # TODO: Adicionar remember_me
+        return redirect(url_for('home'))
+    return render_template('forms/login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
---
+++ forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import TextField, PasswordField, SelectField
+from wtforms.validators import DataRequired, EqualTo, Length, Email, NumberRange
+import email_validator
+
+class RegisterForm(FlaskForm):
+    id = TextField('Código A (somente números)', validators=[DataRequired(), Length(min=5, max=5)])
+    name = TextField('Nome',                     validators=[DataRequired(), Length(min=6, max=60)])
+    email = TextField('Email',                   validators=[DataRequired(), NumberRange(10000, 99999), Email()])
+
+
+class LoginForm(FlaskForm):
+    id = TextField('Código A',                   validators=[DataRequired()])
+    password = PasswordField('Senha',            validators=[DataRequired()])
+
+
+class ForgotForm(FlaskForm):
+    id = TextField('Email',                      validators=[DataRequired(), Length(min=6, max=40), Email()])
+
+class QueryForm(FlaskForm):
+    ano_mes = SelectField('Ano/mês',             validators=[DataRequired()], choices=[])
+    assessores = SelectField('Assessor',         validators=[DataRequired()], choices=[])
+    tabela = SelectField('Fonte',                 validators=[DataRequired()], choices=[('investimentos', 'Investimentos'),
+                                                                                       ('previdencia', 'Previdência'),
+                                                                                       ('co_corretagem', 'Co-Corretagem'),
+                                                                                       ('bancoXP','Banco XP'),
+                                                                                       ('incentivo_previdencia', 'Incentivo Previdência')])
